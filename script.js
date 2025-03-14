27059abd8a03197235a9e8348080e0755212daa7
Jsuckava <147977927+Jsuckava@users.noreply.github.com>
2025-03-15 02:24:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+ class script{
+ 
+    static function(String){
+        Seatwork.html != undefined ? getComputedStyle(h1, "font-weight", undefined)
+         : getComputedStyle(h1, "font-weight", String.substring(   h1.lengt, h1.length)).setAttribute;
+    }
+}
